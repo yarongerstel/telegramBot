d6d14989e4449684bf2de9d15bf1e186ad1712d0
yaron <yarongerstel@windowslive.com>
2022-01-09 00:16:32 +0200

    bot

---
+++ automation_fill.py
@@ -0,0 +1,117 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import time
+
+
+def run_web(id, year, specialty):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get('https://e-services.clalit.co.il/onlinewebquick/nvgq/tamuz/he-il')
+
+    id_box = driver.find_element(By.XPATH, '//*[@id="ctl00_ctl00_cphBody_bodyContent_ucQuickLogin_userId"]')
+    # input id from the user
+    id_box.send_keys(id)
+
+    year_box = driver.find_element(By.XPATH, '//*[@id="ctl00_ctl00_cphBody_bodyContent_ucQuickLogin_userYearOfBirth"]')
+    # input tear from the user
+    year_box.send_keys(year)
+
+    continue_button = driver.find_element(By.XPATH,
+                                          '//*[@id="ctl00_ctl00_cphBody_bodyContent_ucQuickLogin_btnLogin_lblInnerText"]')
+    continue_button.click()
+
+    time.sleep(1)
+    driver.get('https://e-services.clalit.co.il/OnlineWebQuick/QuickServices/Tamuz/TamuzTransferContentByService.aspx')
+    driver.find_element(By.ID, "ProfessionVisitButton").click()
+    select = Select(driver.find_element(By.NAME, 'SelectedSpecializationCode'))
+    switcher = {
+        "אורטופדיה": "58",
+        "אף אוזן גרון": "62",
+        "חירורג שד": "501",
+        "נשים": "63",
+        "עור": "31",
+        "עיניים": "61"
+    }
+    specialty_number = switcher.get(specialty)
+    select.select_by_value(f'{specialty_number}')
+    driver.find_element(By.XPATH, '//*[@id="professionSection"]/div[2]/div[1]/table/tbody/tr[4]/td[3]/input').click()
+    time.sleep(1)
+
+    # if pop banner about faraway queue close that
+    try:
+        driver.find_element(By.XPATH, '//*[@id="CloseButton"]').click()
+    except:
+        pass
+
+    times = []  # str of all the free queue's
+    name_doctor = ""
+    date = ""
+    location = ""
+
+    try:
+        # the date
+        date = driver.find_element(By.XPATH, '//*[@id="diariesList"]/li[1]/div[1]/div[2]/div[3]').text
+        # name of doctor
+        name_doctor = driver.find_element(By.XPATH, '//*[@id="diariesList"]/li[1]/div[1]/div[2]/div[1]').text
+    except:
+        pass
+    # the location
+    location = driver.find_element(By.XPATH, '//*[@id="diariesList"]/li[1]/div[1]/div[3]/div[1]/a').text
+
+    driver.find_element(By.XPATH, '//*[@id="diariesList"]/li[1]/div[3]/div[2]/div/a[1]').click()
+
+    # find all free queue's:
+
+    # morning
+    driver.find_element(By.XPATH, '//*[@id="filter-wrapper"]/div[2]/ul/li[1]/a').click()
+    if driver.find_element(By.XPATH, '//*[@id="morning-noAvailableQueues"]').text != "אין תורים בשעות האלו":
+        try:
+            driver.find_element(By.XPATH, '//*[@id="morning-moreVisits"]/a').click()
+        except:
+            pass
+        # Puts all the free time on the list
+        for i in range(1, 20):
+            try:
+                times.append(
+                    driver.find_element(By.XPATH, '//*[@id="morning-panel"]/ ul/li[' + str(i) + ']/ span[1]').text)
+            except:
+                break
+    # noon
+    try:
+        driver.find_element(By.XPATH, '//*[@id="filter-wrapper"]/div[2]/ul/li[2]/a').click()
+        if driver.find_element(By.XPATH, '//*[@id="noon-noAvailableQueues"]/span').text != "אין תורים בשעות האלו":
+            try:
+                driver.find_element(By.XPATH, '//*[@id="noon-moreVisits"]/a').click()
+            except:
+                pass
+            # Puts all the free time on the list
+            for i in range(1, 20):
+                try:
+                    times.append(
+                        driver.find_element(By.XPATH, '//*[@id="noon-panel"]/ ul/li[' + str(i) + ']/ span[1]').text)
+                except:
+                    break
+    except:
+        pass
+
+    # evening
+    try:
+        driver.find_element(By.XPATH, '//*[@id="filter-wrapper"]/div[1]/ul/li[3]/a').click()
+        if driver.find_element(By.XPATH, '//*[@id="evening-noAvailableQueues"]').text != "אין תורים בשעות האלו":
+            try:
+                driver.find_element(By.XPATH, '//*[@id="evening-moreVisits"]/a').click()
+            except:
+                pass
+            # Puts all the free time on the list
+            for i in range(1, 20):
+                try:
+                    times.append(
+                        driver.find_element(By.XPATH, '//*[@id="evening-panel"]/ ul/li[' + str(i) + ']/ span[1]').text)
+                except:
+                    break
+    except:
+        pass
+
+    return date, location, name_doctor, times
---
+++ general.py
@@ -0,0 +1,81 @@
+from telebot import TeleBot, types
+import automation_fill
+import sys
+import datetime
+import time
+
+times = []
+with open("key.txt", "r") as key:
+    API_KEY = key.read()
+
+bot = TeleBot(API_KEY)
+user_information = []
+
+
+@bot.message_handler(['start'])
+def msg_start(msg):
+    bot.send_message(msg.chat.id, "ברוך הבא")
+    bot.send_message(msg.chat.id, "מהו מספר הזהות שלך?")
+
+
+@bot.message_handler(content_types=['text'])
+def msg_handler(msg):
+    if len(user_information) == 0:
+        while len(msg.text) != 9 or not msg.text.isnumeric():
+            bot.send_message(msg.chat.id, "כתוב תז ב-9 ספרות")
+            return
+        userid = msg.text
+        user_information.append(["id", userid])
+        bot.send_message(msg.chat.id, "יופי. באיזה שנה נולדת?")
+
+    if len(user_information) == 1:
+        while len(msg.text) != 4 or not msg.text.isnumeric():
+            bot.send_message(msg.chat.id, "כתוב שנה ב-4 ספרות")
+            return
+        useryear = msg.text
+        user_information.append(["year", useryear])
+        bot.send_message(msg.chat.id, "לפני איזה תאריך תרצה את התור?")
+
+    if len(user_information) == 2:
+
+        while len(msg.text) != 10 or not msg.text[:2].isnumeric() or not msg.text[3:5].isnumeric() or not msg.text[
+                                                                                                          6:].isnumeric():
+            bot.send_message(msg.chat.id, "כתוב בתבנית של : שנה.חודש.יום ")
+            return
+        d = datetime.datetime(int(msg.text[6:]), int(msg.text[3:5]), int(msg.text[:2]))
+        user_information.append(["date", d])
+
+    if len(user_information) == 3:
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name
+                       in ['נשים', 'אף אוזן גרון', 'אורטופדיה']])
+        keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name
+                       in ['חירורג שד', 'עיניים', 'עור']])
+        msg = bot.send_message(msg.chat.id, 'בחר התמחות', reply_markup=keyboard)
+        user_information.append(["name", keyboard])
+
+    @bot.callback_query_handler(func=lambda c: True)
+    def inline(c):
+        flag = True
+        bot.send_message(c.message.chat.id, c.data)
+        bot.send_message(c.message.chat.id, "מחפש אחר תור קרוב...")
+        while (flag):
+            date, location, name_doctor, times = automation_fill.run_web(user_information[0][1], user_information[1][1],
+                                                                         c.data)
+            d = date[-10:]
+            the_date = datetime.datetime(int(d[6:]), int(d[3:5]), int(d[:2]))
+            # if find queue early print and stop , else keep searching
+            if (user_information[2][1] > the_date):
+                bot.send_message(c.message.chat.id, date)
+                bot.send_message(c.message.chat.id, location)
+                bot.send_message(c.message.chat.id, name_doctor)
+                for t in times:
+                    bot.send_message(c.message.chat.id, t)
+                flag = False
+            time.sleep(600)
+
+        # strting from the begining
+        user_information.clear()
+
+
+bot.polling()
